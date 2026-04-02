26251ffbfc9bbb22b08416fe2fcf63d2824522b2
daril2work <daril2work@gmail.com>
2026-04-03 06:58:24 +0700

    revert: rollback vite config to yesterday's state

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'force-js-mime-type',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url && req.url.match(/\.(ts|tsx)$/)) {
-            res.setHeader('Content-Type', 'application/javascript');
-          }
-          next();
-        });
-      }
-    }
-  ],
-  server: {
-    host: true,
-  },
+  plugins: [react()],
 })
